47ffb6aac6bdf573d65fd90198856f25542e1c0d
anaspicko <rharrass.anas02@gmail.com>
2024-04-11 21:36:53 +0000

    Exclude env directory from version control

---
+++ myproject/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-+a0z3atzc!7&bd4-xv_zrkqbubregl-5sl)+^72g@!eydf#ewl"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
